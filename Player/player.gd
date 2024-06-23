7a222a67eb408f7b11e7af131bd52fa175897d7e
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-06-23 18:56:51 -0400

    Fixed issue of nodes not showing in heirachy

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 100
+@export var speed = 200;
 
 
 func _physics_process(delta):
@@ -10,17 +10,17 @@ func _physics_process(delta):
 
 
 func playerMovement(delta):
-	if Input.is_action_just_pressed("Right"):
+	if Input.is_action_pressed("Right"):
 		velocity.y = 0
-		velocity.x = speed * delta
-	elif Input.is_action_just_pressed("Left"):
+		velocity.x = speed
+	elif Input.is_action_pressed("Left"):
 		velocity.y = 0
-		velocity.x = -speed * delta
-	elif Input.is_action_just_pressed("Up"):
-		velocity.y = -speed * delta
+		velocity.x = -speed
+	elif Input.is_action_pressed("Up"):
+		velocity.y = -speed
 		velocity.x = 0
-	elif Input.is_action_just_pressed("Down"):
-		velocity.y = speed * delta
+	elif Input.is_action_pressed("Down"):
+		velocity.y = speed
 		velocity.x = 0
 	else:
 		velocity.y = 0
