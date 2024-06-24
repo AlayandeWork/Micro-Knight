63f16fb82407dd7a2c80ba132f2e3d508f71c52f
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-06-24 06:25:25 -0400

    Adding animations to player node

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 200;
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 func _physics_process(delta):
@@ -13,9 +14,13 @@ func playerMovement(delta):
 	if Input.is_action_pressed("Right"):
 		velocity.y = 0
 		velocity.x = speed
+		animated_sprite_2d.play("run_right")
+		
 	elif Input.is_action_pressed("Left"):
 		velocity.y = 0
 		velocity.x = -speed
+		animated_sprite_2d.flip_h
+		animated_sprite_2d.play("run_right")
 	elif Input.is_action_pressed("Up"):
 		velocity.y = -speed
 		velocity.x = 0
