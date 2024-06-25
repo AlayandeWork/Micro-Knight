4cd7f8878534ce418161e0df623a48f7eb4e2dd2
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-06-25 11:04:14 -0400

    Added idle animation based on last direction faced

--- Player/player.gd
+++ Player/player.gd
@@ -1,38 +1,70 @@
 extends CharacterBody2D
 
-@export var speed = 200;
-@onready var animated_sprite_2d = $AnimatedSprite2D
+var speed = 150
+var direction = "none"
+@onready var anim = $AnimatedSprite2D
 
+func _ready():
+	anim.play("idle_down")
 
 func _physics_process(delta):
-	playerMovement(delta)
+	player_movement(delta)
 
-
-
-
-func playerMovement(delta):
-	if Input.is_action_pressed("Right"):
+func player_movement(delta):
+	if Input.is_action_pressed("ui_right"):
+		direction = "right"
+		animationplay(1)
 		velocity.y = 0
 		velocity.x = speed
-		animated_sprite_2d.play("run_right")
 		
-	elif Input.is_action_pressed("Left"):
+	elif Input.is_action_pressed("ui_left"):
+		direction = "left"
+		animationplay(1)
 		velocity.y = 0
 		velocity.x = -speed
 		
-	elif Input.is_action_pressed("Up"):
+	elif Input.is_action_pressed("ui_up"):
+		direction = "up"
+		animationplay(1)
 		velocity.y = -speed
 		velocity.x = 0
-		animated_sprite_2d.play("run_up")
-	elif Input.is_action_pressed("Down"):
+		
+	elif Input.is_action_pressed("ui_down"):
+		direction = "down"
+		animationplay(1)
 		velocity.y = speed
 		velocity.x = 0
 	else:
-		velocity.y = 0
-		animated_sprite_2d.play("idle_down")
-		velocity.x = 0
-		animated_sprite_2d.play("idle_right")
-		
-	
+		animationplay(0)
+		velocity = Vector2.ZERO
 
 	move_and_slide()
+
+func animationplay(move):
+	if direction == "right":
+		anim.flip_h = false
+		if move == 1:
+			anim.play("run_right")
+		else:
+			anim.play("idle_right")
+			
+	elif direction == "left":
+		anim.flip_h = true
+		if move == 1:
+			anim.play("run_right")
+		else:
+			anim.play("idle_right")
+			
+	elif direction == "up":
+		anim.flip_h = false
+		if move == 1:
+			anim.play("run_up")
+		else:
+			anim.play("idle_up")
+			
+	elif direction == "down":
+		anim.flip_h = false
+		if move == 1:
+			anim.play("run_down")
+		else:
+			anim.play("idle_down")
