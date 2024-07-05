cc74ffaa84f59a5f9d17bb087e97eabc70b215d2
AbdulMatin Alayande Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-07-05 07:30:58 -0400

    Updates
    1. Enemy follows player
    2. Enemy Y-Sorting
    3. Enemy collides with player

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,28 +1,29 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var playerIsChasing = false;
+var enemySpeed = 50;
+var player = null
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if playerIsChasing:
+		position += (player.position - position)/enemySpeed
+		$AnimatedSprite2D.play("enemy_run")
+		
+		
+		if (player.position.x - position.x)<0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+			
+			
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("enemy_idle")
+		
+func _on_enemy_detection_body_entered(body):
+	player = body
+	playerIsChasing = true
 
-	move_and_slide()
+
+func _on_enemy_detection_body_exited(body):
+	player = null
+	playerIsChasing = false
