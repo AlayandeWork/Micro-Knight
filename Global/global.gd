77e0aadc2b5a0ec8086cf32bee59dd7a3b5eb96d
AbdulMatin Alayande Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-07-08 15:41:14 -0400

    Update
    1. Scene Transition
    2. Constant player position after scene change
    3. New Level

--- Global/global.gd
+++ Global/global.gd
@@ -1,3 +1,24 @@
 extends Node
 
 var playerCurrentlyAttacking = false
+
+var currentScene = "level"
+var isTransitioning = false
+
+var playerExitX = 717
+var playerExitY = 164
+var playerEnterX = 217
+var playerEnterY = 156
+
+
+var gameStart = true
+
+
+
+func finishSceneTransition():
+	if isTransitioning == true:
+		isTransitioning = false
+		if currentScene == "level":
+			currentScene = "level_2"
+		else:
+			currentScene = "level"
--- World/Level/level_2.gd
+++ World/Level/level_2.gd
@@ -3,13 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if Globle.gameStart == true:
-		$Player.position.x = Globle.playerEnterX
-		$Player.position.y = Globle.playerEnterY
-	else:
-		$Player.position.x = Globle.playerExitX
-		$Player.position.y = Globle.playerExitY
-		
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,20 +11,20 @@ func _process(delta):
 	changeScene()
 
 
-func _on_transition_area_body_entered(body):
+func _on_transition_area_2_body_entered(body):
 	if body.has_method("player"):
 		Globle.isTransitioning = true
 		print ("player has entered transition area")
 
 
-func _on_transition_area_body_exited(body):
+func _on_transition_area_2_body_exited(body):
 	if body.has_method("player"):
 		Globle.isTransitioning = false
-		print ("player has exited transition area")
+		print ("player has entered transition area")
+
 
 func changeScene():
 	if Globle.isTransitioning == true:
 		if Globle.currentScene == "level":
-			get_tree().change_scene_to_file("res://World/Level/level_2.tscn")
-			Globle.gameStart = false
+			get_tree().change_scene_to_file("res://World/Level/level.tscn")
 			Globle.finishSceneTransition()
